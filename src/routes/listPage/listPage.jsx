95c0667e106c3c2d31aa6d9c65f6285b9376bc29
priyanshu <priyanshumandloigurukul@gmail.com>
2024-05-14 23:13:31 +0530

    Completed the list Page Portion

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,7 +1,28 @@
 import "./listPage.scss";
 
+import Card from "../../components/card/Card";
+import Filter from "../../components/filter/Filter";
+import Map from "../../components/map/Map";
+import { listData } from "../../lib/dummydata";
+
 function ListPage() {
-  return <div className="listPage">ListPage</div>;
+  const data = listData;
+
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">
+        <Map items={data} />
+      </div>
+    </div>
+  );
 }
 
 export default ListPage;
