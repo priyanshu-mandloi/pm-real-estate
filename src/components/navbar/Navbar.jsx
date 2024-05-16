c093bad25868c62b7db4af24199cbec9d51f61de
priyanshu <priyanshumandloigurukul@gmail.com>
2024-05-16 20:13:09 +0530

    Completed the chat functionality in the application

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
       <div className="left">
         <a href="/" className="logo">
           <img src="/logo.png" alt="" />
-          <span>LamaEstate</span>
+          <span>PMEstate</span>
         </a>
         <a href="/">Home</a>
         <a href="/">About</a>
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <SocketContextProvider>
+        <App />
+      </SocketContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
