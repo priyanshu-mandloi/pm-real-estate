e72e3fdc939f1b8fbdb73725764a510897bef187
priyanshu <priyanshumandloigurukul@gmail.com>
2024-07-01 03:54:29 +0530

    updated the funtionality

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   // baseURL: "http://localhost:8800/api",
-  baseURL: "https://pm-real-estate-backend.onrender.com/api",
+  baseURL: "https://backend-6vnm.onrender.com/api",
   withCredentials: true,
 });
 
